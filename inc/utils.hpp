13be77824f05c9183f38d88ab4955c983629873b
Marin Becker <mbecker@student.42.fr>
2025-02-27 16:22:33 +0100

    added getTestRequest() in utils.cpp

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: mbecker <mbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:33:23 by mbecker           #+#    #+#             */
-/*   Updated: 2025/02/13 18:01:33 by mbecker          ###   ########.fr       */
+/*   Updated: 2025/02/27 15:50:43 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 #include "libs.h"
 
+
 // UTILS.CPP
 
 std::string itostr(int num);
 int	countOccurrences(const string s, char c);
+string getTestRequest();
+
 
 // CONFIG_PRINT.CPP
 
--- src/utils.cpp
+++ src/utils.cpp
@@ -6,7 +6,7 @@
 /*   By: mbecker <mbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 15:30:39 by mbecker           #+#    #+#             */
-/*   Updated: 2025/02/13 18:01:36 by mbecker          ###   ########.fr       */
+/*   Updated: 2025/02/27 15:50:29 by mbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,20 @@ int	countOccurrences(const string s, char c)
 			count++;
 
 	return (count);
+}
+
+string getTestRequest()
+{
+	string input;
+	string filepath;
+	cout << "Enter the path to the file to test, or empty for default (tools/message/request/valid/simple.txt): ";
+	getline(cin, filepath);
+	if (filepath.empty())
+		filepath = "tools/message/request/valid/simple.txt";
+	ifstream file(filepath.c_str());
+	for (string tmp; getline(file, tmp);)
+		input += tmp + "\r\n";
+	file.close();
+
+	return input;
 }
